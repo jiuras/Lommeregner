93f30747352b09ed368798519f6486c88d3b3e35
jiuras <jiuras@users.noreply.github.com>
2020-09-25 09:39:12 +0200

    HTML og JS tilføjet til repository

---
+++ script.js
@@ -0,0 +1,19 @@
+function addition() {
+    let x = document.getElementById("txt1").value;
+    let y = document.getElementById("txt2").value;
+
+    let result = x + y;
+    document.getElementById("result").value = result;
+}
+
+function subtraction() {
+
+}
+
+function multiplication() {
+
+}
+
+function division() {
+
+}
