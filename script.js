823c8763293628a5d3b724b24db65287d12dfee8
jiuras <jiuras@users.noreply.github.com>
2020-09-25 10:24:09 +0200

    Tilføjet de forskellige regnemetoder

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ function addition() {
 }
 
 function subtraction() {
+    let x = parseInt(document.getElementById("txt1").value);
+    let y = parseInt(document.getElementById("txt2").value);
 
+    let result = x - y;
+    document.getElementById("result").value = result;
 }
 
 function multiplication() {
+    let x = parseInt(document.getElementById("txt1").value);
+    let y = parseInt(document.getElementById("txt2").value);
 
+    let result = x * y;
+    document.getElementById("result").value = result;
 }
 
 function division() {
+    let x = parseInt(document.getElementById("txt1").value);
+    let y = parseInt(document.getElementById("txt2").value);
 
+    let result = x / y;
+    document.getElementById("result").value = result;
 }
