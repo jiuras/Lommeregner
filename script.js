708e3e4f13c351c216e12080813d7f79123855c8
jiuras <57357540+jiuras@users.noreply.github.com>
2020-09-26 14:33:13 +0200

    Ændring af onClick metode

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
+function doTheMath(method) {
+    let x = parseFloat(document.getElementById("txt1").value);
+    let y = parseFloat(document.getElementById("txt2").value);
+
+    if (method = addition)
+        let result = x + y;
+
+    else if (method = subtraction)
+        let result = x - y;
+
+    else if (method = multiplication)
+        let result = x * y;
+        
+    else if (method = division)
+        let result = x / y;
+}
+
+
+
+
 function addition() {
-    let x = parseInt(document.getElementById("txt1").value);
-    let y = parseInt(document.getElementById("txt2").value);
+    let x = parseFloat(document.getElementById("txt1").value);
+    let y = parseFloat(document.getElementById("txt2").value);
 
     let result = x + y;
     document.getElementById("result").value = result;
 }
 
 function subtraction() {
-    let x = parseInt(document.getElementById("txt1").value);
-    let y = parseInt(document.getElementById("txt2").value);
+    let x = parseFloat(document.getElementById("txt1").value);
+    let y = parseFloat(document.getElementById("txt2").value);
 
     let result = x - y;
     document.getElementById("result").value = result;
 }
 
 function multiplication() {
-    let x = parseInt(document.getElementById("txt1").value);
-    let y = parseInt(document.getElementById("txt2").value);
+    let x = parseFloat(document.getElementById("txt1").value);
+    let y = parseFloat(document.getElementById("txt2").value);
 
     let result = x * y;
     document.getElementById("result").value = result;
 }
 
 function division() {
-    let x = parseInt(document.getElementById("txt1").value);
-    let y = parseInt(document.getElementById("txt2").value);
+    let x = parseFloat(document.getElementById("txt1").value);
+    let y = parseFloat(document.getElementById("txt2").value);
 
     let result = x / y;
     document.getElementById("result").value = result;
