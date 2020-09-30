3efa3b782ec352e1ee4c6769b00340959059df0a
jiuras <jiuras@users.noreply.github.com>
2020-09-30 12:53:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-/*function doTheMath(method) {
+function doTheMath(method) {
     let x = parseFloat(document.getElementById("txt1").value);
     let y = parseFloat(document.getElementById("txt2").value);
 
-    if (method = addition)
+    if (method === "addition")
         let result = x + y;
 
-    else if (method = subtraction)
+    else if (method === "subtraction")
         let result = x - y;
 
-    else if (method = multiplication)
+    else if (method === "multiplication")
         let result = x * y;
         
-    else if (method = division)
+    else if (method === "division")
         let result = x / y;
 }
-*/
+
 
 //Få overstående kode til at virke
 
